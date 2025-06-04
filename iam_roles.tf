a86f02cd951d7eccdbd62f2140bb7085cf835c2a
SathyaVihaan <asathyavihaan@gmail.com>
2025-06-04 18:06:23 +0530

    Create iam_roles.tf

---
+++ iam_roles.tf
@@ -0,0 +1,21 @@
+resource "aws_iam_role" "github_oidc_role" {
+  name = "github-actions-oidc-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        },
+        Action = "sts:AssumeRoleWithWebIdentity",
+        Condition = {
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:SathyaVihaan/AWS_ELB_UC-1:*"
+          }
+        }
+      }
+    ]
+  })
+}
